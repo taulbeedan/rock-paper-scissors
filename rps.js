9c09d237bc52654f9003549b17222704f110083f
taulbeedan <taulbeedan@gmail.com>
2021-10-16 20:04:09 -0500

    added function 'computerPlay()'

--- rps.js
+++ rps.js
@@ -1,1 +1,10 @@
-console.log("Hello!")
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3); //generate a random integer between 0 & 2
+    if (randomNumber == 0) {
+        return "Rock!";  //return string "Rock!" for 0
+    } else if (randomNumber == 1) {
+        return "Paper!";  //return string "Paper" for 1
+    } else {
+        return "Scissors!";  //return string "Scissors" otherwise (2 should be only other option)
+    }
+}
