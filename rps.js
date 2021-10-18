1fcb836be13633080d290f7d2c8524ea7260c162
taulbeedan <taulbeedan@gmail.com>
2021-10-18 10:51:16 -0500

    created function playRound(), added win counters

--- rps.js
+++ rps.js
@@ -1,10 +1,43 @@
+let userWinCounter = 0;
+let computerWinCounter = 0;
+
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3); //generate a random integer between 0 & 2
     if (randomNumber == 0) {
-        return "Rock!";  //return string "Rock!" for 0
+        return "ROCK";  //return string "Rock!" for 0
     } else if (randomNumber == 1) {
-        return "Paper!";  //return string "Paper" for 1
+        return "PAPER";  //return string "Paper" for 1
     } else {
-        return "Scissors!";  //return string "Scissors" otherwise (2 should be only other option)
+        return "SCISSORS";  //return string "Scissors" otherwise (2 should be only other option)
     }
+}
+
+function playRound() {
+    let playerEntry = prompt("Rock, paper, or scissors?"); //prompt player to make Entry
+    let playerSelection = playerEntry.toUpperCase();  //make playerEntry ALLCAPS for input case insensitivity
+    let computerSelection = computerPlay();  //sets computerSelection by invoking computerPlay
+    if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {  //compare playerSelection and computerSelection and set response
+        response = "Rock beats Scissors! You Win!";
+        userWinCounter++;
+    } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+        response = "Scissors beat Paper! You Win!";
+        userWinCounter++;
+    } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+        response = "Paper beats Rock! You Win!";
+        userWinCounter++;
+    } else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+        response = "Paper beats Rock. You lose.";
+        computerWinCounter++;
+    } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
+        response = "Rock beats Scissors. You lose.";
+        computerWinCounter++;
+    } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
+        response = "Scissors beats Paper. You lose.";
+        computerWinCounter++;
+    } else if (playerSelection == computerSelection) {
+            response = "It's a tie!";
+    } else {
+        response = "Invalid entry. Please enter only ROCK PAPER or SCISSORS.";
+    }
+    return response;
 }
